2b4e0695e2a954d759022679b3da13fe72cf947f
Андрей Овчаренко <andrey.ovcharenko@branderstudio.com>
2020-11-22 20:28:45 +0200

    delete data if empty

--- src/router.js
+++ src/router.js
@@ -2,20 +2,20 @@ const axios = require('axios');
 const UniversalRouter = require('universal-router');
 
 module.exports = new UniversalRouter([
-  { path: '/home', action: (req) => ({ page: 'home', data: { req, action: 'home' } }) },
+  { path: '/home', action: (req) => ({ page: 'home' }) },
   {
     path: '/user-list',
     action: async (req) => {
       const {data: users} = await axios.get('http://localhost:8080/api/users');
-      return { page: 'user-list', data: { req, users, action: 'user-list' } };
+      return { page: 'user-list', data: { users } };
     }
   },
   {
     path: '/users/:id',
     action: async (req) => {
       const {data: user} = await axios.get(`http://localhost:8080/api/users/${req.params.id}`);
-      return { page: 'user', data: { req, user, action: 'user' } };
+      return { page: 'user', data: { req, user } };
     }
   },
-  { path: '(.*)', action: () => ({ page: 'notFound', data: { action: 'not-found' } }) }
+  { path: '(.*)', action: () => ({ page: 'notFound' }) }
 ])
